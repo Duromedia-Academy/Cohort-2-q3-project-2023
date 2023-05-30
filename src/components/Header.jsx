9a890d9cc21cf04d76ea5d5595dbf387b4f21c37
fatimah140 <fatimahconde14@gmail.com>
2023-05-30 17:18:15 +0100

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,54 +2,29 @@ import React from 'react'
 
 import {FaBars} from "react-icons/fa"
 import { Link } from 'react-router-dom'
-
-const Header = () => {
+const header = () => {
   return (
-    <div className='flex justify-between items-center'>
-      <div className="flex gap-10 items-center">
+    <nav className=''>
 
-      <nav>
-          <h1 className='font-semibold text-3xl pl-8'>teach:able</h1>
+      <div className="flex justify-between items-center">
+      <div className="">
+        <a href="/b" className='text-4xl pl-10'>teach:able</a>
+      </div>
 
-            <ul className='flex gap-10'>
-                <li className='font-normal hover:text-[#21cd9c]'>
-                   <Link to={"/"}>
-                    Features
-                   </Link>
-                </li>
-                <li  className='font-normal hover:text-[#21cd9c]'>
-                   <Link to={"/about"}>
-                    Pricing
-                   </Link>
-                </li>
-                <li className='font-normal hover:text-[#21cd9c]'>
-                   <Link to={"/"}>
-                    Blog
-                   </Link>
-                </li>
-                <li className='font-normal hover:text-[#21cd9c]'>
-                   <Link to={"/"}>
-                    Examples
-                   </Link>
-                </li>
-                <li className='font-normal hover:text-[#21cd9c]'>
-                   <Link to={"/"}>
-                    Resources
-                   </Link>
-                </li>
-            </ul>
-        </nav>
-        </div>
-        <div className="flex gap-6 items-center pr-3">
-          <Link className='underline'>Log in</Link>
-          <button className=' bg-[#1b4942] rounded-md p-4 text-white hover:bg-black'><Link>Start for free</Link></button>
-        </div>
 
-        <div>
-          <FaBars/>
-        </div>
-    </div>
-  )
+
+      <div className=" pt-3  flex pr-3">
+        <a href="/s" className='pr-3'>Start For Free</a>  
+          <FaBars className='text-2xl'/>
+      </div>
+
+      </div>
+
+
+
+
+    </nav>
+  );
 }
-
-export default Header
+ 
+export default header;
