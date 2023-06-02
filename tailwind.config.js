7ca51d26636d5d9abda7dd066920f1fe9da9c327
lord-space <teslimabdullah2@gmail.com>
2023-06-02 15:55:44 -0600

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   mode: 'jit',
-  theme: {
-    extend: {},
+  theme: { 
+   
+    extend: {
+      colors: {
+        'peak':' rgb(34, 34, 34)',
+      },
+      fontSize:{
+        'mag':'1.1rem'
+      },
+      maxWidth:{
+        'ok': '1250px'
+      },
+    },
   },
   plugins: [],
 }
