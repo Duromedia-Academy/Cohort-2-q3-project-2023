62642b354c707066ab215042568e0d0a0c8ef81b
lord-space <teslimabdullah2@gmail.com>
2023-06-04 14:42:16 -0600

    main

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -10,6 +10,9 @@ const Home = () => {
             <h1>welcom to my home page</h1>
             <h1>welcom to my home page</h1>
             <h1>welcom to my home page</h1>
+            <h1>welcom to my home page</h1>
+            <h1>welcom to my home page</h1>
+            <h1>welcom to my home page</h1>
         </div>
     );
 }
