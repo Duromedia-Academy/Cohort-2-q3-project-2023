4868bbd5a20b977ee4b0afa56f678f449dd6523c
lord-space <teslimabdullah2@gmail.com>
2023-06-03 22:00:36 -0600

    footer

--- src/containers/Resources/Downloads/Downloads.js
+++
@@ -1,9 +0,0 @@
-const Downloads = () => {
-    return (
-        <div>
-         <h1>home</h1>
-        </div>
-    );
-}
- 
-export default Downloads;
--- src/containers/index.js
+++ src/containers/index.js
@@ -2,8 +2,3 @@ export {default as Home } from './Home/Home'
 
 export {default as About } from './About/About'
 
-export {default as Downloads } from './Resources/Downloads/Downloads'
-export {default as Events } from './Resources/Events/Events'
-export {default as Knowledge } from './Resources/Knowledge/Knowledge'
-export {default as Podcast } from './Resources/Podcast/Podcast'
-export {default as Teachable } from './Resources/Teachable/Teachable'
